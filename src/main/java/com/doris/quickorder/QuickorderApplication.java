cf28695aceebcd09c7933095ab597a13d0f04af6
274902327@qq.com <274902327@qq.com>
2016-05-01 16:20:49 +0800

    first

---
+++ src/main/java/com/doris/quickorder/QuickorderApplication.java
@@ -0,0 +1,12 @@
+package com.doris.quickorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickorderApplication.class, args);
+	}
+}
